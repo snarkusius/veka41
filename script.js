f4ef409832c036f5790fbca38ff859a0a969d223
snarkusius <marcus@lengquist.se>
2020-10-05 13:43:32 +0200

    hej

---
+++ script.js
@@ -0,0 +1,4 @@
+const button = document.getElementById("button")
+const inputfirld = document.getElementById("inputfield")
+button.onclick = function(){alert("hej")} 
+button.onmouseenter =function(){alert("nope")}
